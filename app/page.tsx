aa6569ade243f53f5c20629133dcc2146067109f
James Holyhead <jholyhead@gmail.com>
2025-01-17 08:57:30 +0000

    formatting tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Chess Ratings Dashboard</h1>
-      <h2 className="text-3xl font-bold mb-4">Enhanced Analytics for English Chess Federation Ratings</h2>
+      <h2 className="text-xl font-bold mb-4">Enhanced Analytics for English Chess Federation Ratings</h2>
       <div className="mb-4">
         <PlayerSearch />
       </div>
