ca7000958b8b8441560abb28860a86474043c67b
James Holyhead <jholyhead@gmail.com>
2025-01-17 00:58:59 +0000

    added Umami tracking code to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="4d71ba17-f234-45d6-b415-a1ace9193a87"
+        />
+      </head>
       <body className={inter.className}>
         <main className="min-h-screen bg-gray-100">
           {children}
@@ -23,4 +31,3 @@ export default function RootLayout({
     </html>
   )
 }
-
