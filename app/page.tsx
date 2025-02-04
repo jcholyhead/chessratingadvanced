0b6d94a9668659553c149431127e6a2b03ee4966
James Holyhead <jholyhead@gmail.com>
2025-02-04 18:32:53 +0000

    maintenance

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import ChessResultsTable from "@/components/ChessResultsTable"
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <h2 className="text-xl font-semibold mb-4">Enhanced Analytics for English Chess Federation Ratings</h2>
+      <h1>Under Maintenance, we'll be back as soon as possible</h1>
+      {/* <h2 className="text-xl font-semibold mb-4">Enhanced Analytics for English Chess Federation Ratings</h2>
       <div className="mb-4">
         <Suspense fallback={<div>Loading player search...</div>}>
           <PlayerSearchWrapper />
@@ -13,7 +14,7 @@ export default function Home() {
       </div>
       <Suspense fallback={<div>Please search for a player to view their results.</div>}>
         <ChessResultsTable initialPlayerCode={null} />
-      </Suspense>
+      </Suspense> */}
     </div>
   )
 }
