c76cfd9c93b41ed86d2d6b97872fab48e373838d
James Holyhead <jholyhead@gmail.com>
2025-02-04 18:02:08 +0000

    maintenance

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import { Suspense } from "react"
-import PlayerSearchWrapper from "@/components/PlayerSearchWrapper"
-import ChessResultsTable from "@/components/ChessResultsTable"
+// import PlayerSearchWrapper from "@/components/PlayerSearchWrapper"
+// import ChessResultsTable from "@/components/ChessResultsTable"
 
 export default function Home() {
   return (
     <div className="container mx-auto">
+      <h2 className="text-xl font-semibold mb-4">Enhanced Analytics for English Chess Federation Ratings</h2>
+      <div className="mb-4">
+        <Suspense fallback={<div>Loading player search...</div>}>
+        <h1>Under Maintenance Back Soon!</h1>
+
+        </Suspense>
+      </div>
+      <Suspense fallback={<div>Please search for a player to view their results.</div>}>
       <h1>Under Maintenance Back Soon!</h1>
+      </Suspense>
     </div>
   )
 }
