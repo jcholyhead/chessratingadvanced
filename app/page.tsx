0a3b716fad0f1847d9ed4118ff7a2babb6673a34
James Holyhead <jholyhead@gmail.com>
2025-02-04 17:56:27 +0000

    maintenance

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import ChessResultsTable from "@/components/ChessResultsTable"
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <h1>Under Maintenance Back Really Soon!</h1>
+      <h1>Under Maintenance Back Soon!</h1>
     </div>
   )
 }
